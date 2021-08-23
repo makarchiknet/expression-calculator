a5e9b048cfdaa67de201233ddc37e82858cfb9b5
Kate Makarova <makarchiknet@gmail.com>
2021-08-23 20:41:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.split(' ').join('');
+
+    let brackets = 0;
+
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === '(') {
+            brackets += 1;
+        } else if (expr[i] === ')') {
+            brackets -= 1;
+        }
+    }
+    if (brackets !== 0) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+    if (expr.includes('/0')) {
+        throw new Error('TypeError: Division by zero.');
+    }
+
+    return new Function(`return ${expr}`)();
 }
 
 module.exports = {
     expressionCalculator
-}
+};
